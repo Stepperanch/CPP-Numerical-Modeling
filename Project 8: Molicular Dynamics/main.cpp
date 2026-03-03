a97b65d63b650753096f2b351673891ff921515d
Nels Buhrley <nelsbuhrley@gmail.com>
2026-03-02 23:28:59 -0700

    Built molecular dynamics simulation - 2D Lennard-Jones MD with Verlet integration

---
+++ Project 8: Molicular Dynamics/main.cpp
@@ -0,0 +1,31 @@
+#include "processing.h"
+
+int main() {
+    // Example usage of the MolucularSystem class
+    std::vector<std::array<double, MolucularSystem::d>> initialPositions;
+
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+
+            initialPositions.push_back({i * 1.1 + 5.0 + rand() / RAND_MAX * 0.7 , j * 1.1 + 5.0 + rand() / RAND_MAX * 0.7});  // Place particles in a grid with spacing of 1.5 sigma
+        }
+    }
+
+
+    unsigned int timeSteps = 300000;
+    double finalTime = 150.0;
+    float boxSize = 50.0;
+
+
+
+
+    MolucularSystem system(initialPositions, buildEnergyFunction(timeSteps), timeSteps, finalTime, boxSize);
+    std::cout << "Running simulation with " << system.numParticles << " particles for " << system.timeSteps << " time steps on " << omp_get_num_threads() << " threads." << std::endl;
+    system.runSimulation();
+    std::cout << "Simulation complete. Saving results..." << std::endl;
+
+    system.save();
+    std::cout << "Results saved to NPZ file." << std::endl;
+
+    return 0;
+}
